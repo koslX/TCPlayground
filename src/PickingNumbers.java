554c2458cb75eff08d5b1826fce319549e3542b0
Karol <koslows@gmail.com>
2018-09-11 18:17:00 +0200

    Ammending PickingNumbers Formatting

--- src/PickingNumbers.java
+++ src/PickingNumbers.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by Karol on 30/08/2018.
  *
@@ -8,39 +9,37 @@
 import java.util.Arrays;
 
 public class PickingNumbers {
-	
-    // Complete the pickingNumbers function below.
-    static int pickingNumbers(int[] a) {
-    	
-    	Arrays.sort(a);
-    	
-    	int start = 0;
-    	int mid = 0;
-    	int maxL = 1;
-    	
-    	for(int i = 1; i < a.length; i++)
-    	{
 
+	// Complete the pickingNumbers function below.
+	static int pickingNumbers(int[] a) {
 
-    		if(a[i] > a[i-1])
-    		{
-    			if(a[i] > a[i-1]+1)
-    				start = i;
+		Arrays.sort(a);
 
-    			else
-    				start = mid;
-    			
+		int start = 0;
+		int mid = 0;
+		int maxL = 1;
+
+		for (int i = 1; i < a.length; i++) {
+
+			if (a[i] > a[i - 1]) {
+				if (a[i] > (a[i - 1] + 1)) {
+					start = i;
+				} else {
+					start = mid;
+				}
+
 				mid = i;
-    		}
+			}
 
-			if(i-start+1 > maxL)
-				maxL = i-start + 1;
-    		
-    	}
-    	
-    	return maxL;
+			if (((i - start) + 1) > maxL) {
+				maxL = (i - start) + 1;
+			}
 
-    }
+		}
+
+		return maxL;
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
